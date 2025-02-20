a23cabd6ede5f05a3a6a2748f7cb5ba3ed87f44c
E-milien <emilien.roukine@gmail.com>
2025-02-20 14:31:16 +0100

    changes

--- dev.py
+++ dev.py
@@ -5,7 +5,19 @@ import time
 
 app = Flask(__name__)
 
-MOCK_ROOMS = ["d251", "d351", "d360"]
+IUT_LOCATION = "iut"
+TETRAS_LOCATION = "tetras"
+
+IUT_ROOMS = ["d251", "d351", "d360"]
+TETRAS_ROOMS = ["02", "11", "19"]
+
+THRESHOLDS = {
+    "co2_level": 1000,
+    "temperature": (20, 26),
+    "humidity": (30, 60),
+    "loudness": 50,
+    "smoke_density": 0
+}
 
 def generate_sensor_data(sensor_type, days=30):
     data = {
@@ -60,22 +72,39 @@ TYPESENSOR = [
     ("binary", "binary_sensor")
 ]
 
-MOCK_SENSORS = {
+IUT_SENSORS = {
     room: {
         f"{room}_{sensor_type}": f"{sensor_type.replace('_', ' ').title()} Sensor"
         for sensor_type, _ in TYPESENSOR
-    } for room in MOCK_ROOMS
+    } for room in IUT_ROOMS
 }
 
-def get_sensors_list_for_room(room):
-    if room not in MOCK_ROOMS:
-        return None
-    return list(MOCK_SENSORS[room].keys())
+TETRAS_SENSORS = {
+    room: {
+        f"{room}_{sensor_type}": f"{sensor_type.replace('_', ' ').title()} Sensor"
+        for sensor_type, _ in TYPESENSOR
+    } for room in TETRAS_ROOMS
+}
 
-def get_sensor_data(sensor_id):
-    room = next((room for room in MOCK_ROOMS if sensor_id.startswith(room)), None)
-    if not room or sensor_id not in MOCK_SENSORS[room]:
-        return None
+def get_sensors_list_for_room(room, location):
+    if location == IUT_LOCATION:
+        if room not in IUT_ROOMS:
+            return None
+        return list(IUT_SENSORS[room].keys())
+    else: 
+        if room not in TETRAS_ROOMS:
+            return None
+        return list(TETRAS_SENSORS[room].keys())
+
+def get_sensor_data(sensor_id, location):
+    if location == IUT_LOCATION:
+        room = next((room for room in IUT_ROOMS if sensor_id.startswith(room)), None)
+        if not room or sensor_id not in IUT_SENSORS[room]:
+            return None
+    else:
+        room = next((room for room in TETRAS_ROOMS if sensor_id.startswith(room)), None)
+        if not room or sensor_id not in TETRAS_SENSORS[room]:
+            return None
     
     sensor_type = sensor_id.split('_', 1)[1]
     measurement = next((m for t, m in TYPESENSOR if t == sensor_type), None)
@@ -94,40 +123,68 @@ def get_sensor_data(sensor_id):
 
 @app.route("/api/sensors")
 def get_all_sensors():
-    sensors_dict = {}
-    for room in MOCK_ROOMS:
-        for sensor_id, sensor_name in MOCK_SENSORS[room].items():
-            for sensor_type, measurementType in TYPESENSOR:
-                if sensor_type in sensor_id:
-                    measurement = measurementType
-            sensors_dict[sensor_id] = {
-                'measurement': measurement,
-                'domain': 'sensor',
-                'friendly_name_str': sensor_name,
-                'room': room,
-            }
-    return jsonify(sensors_dict), 200
+    location = request.args.get('location', 'iut')
+    if location == IUT_LOCATION:
+        sensors_dict = {}
+        for room in IUT_ROOMS:
+            for sensor_id, sensor_name in IUT_SENSORS[room].items():
+                for sensor_type, measurementType in TYPESENSOR:
+                    if sensor_type in sensor_id:
+                        measurement = measurementType
+                sensors_dict[sensor_id] = {
+                    'measurement': measurement,
+                    'domain': 'sensor',
+                    'friendly_name_str': sensor_name,
+                    'room': room,
+                }
+        return jsonify(sensors_dict), 200
+    else: 
+        sensors_dict = {}
+        for room in TETRAS_ROOMS:
+            for sensor_id, sensor_name in TETRAS_SENSORS[room].items():
+                for sensor_type, measurementType in TYPESENSOR:
+                    if sensor_type in sensor_id:
+                        measurement = measurementType
+                sensors_dict[sensor_id] = {
+                    'measurement': measurement,
+                    'domain': 'sensor',
+                    'friendly_name_str': sensor_name,
+                    'room': room,
+                }
+        return jsonify(sensors_dict), 200
 
 @app.route("/api/rooms")
 def get_all_rooms():
-    return jsonify([{"name": room} for room in MOCK_ROOMS]), 200
+    location = request.args.get('location', 'iut')
+    if location == IUT_LOCATION:
+        return jsonify([{"name": room} for room in IUT_ROOMS]), 200
+    else:
+        return jsonify([{"name": room} for room in TETRAS_ROOMS]), 200
 
 @app.route("/api/room/<room>/sensor-list")
 def get_sensors_by_room(room):
-    if room not in MOCK_ROOMS:
-        return {"error": "Room not found"}, 404
-    return jsonify(list(MOCK_SENSORS[room].keys())), 200
+    location = request.args.get('location', 'iut')
+    if location == IUT_LOCATION:
+        if room not in IUT_ROOMS:
+            return {"error": "Room not found"}, 404
+        return jsonify(list(IUT_SENSORS[room].keys())), 200
+    else:
+        if room not in TETRAS_ROOMS:
+            return {"error": "Room not found"}, 404
+        return jsonify(list(TETRAS_SENSORS[room].keys())), 200
 
 @app.route("/api/sensor/<sensor_id>")
 def get_data_by_sensor_id(sensor_id):
-    data = get_sensor_data(sensor_id)
+    location = request.args.get('location', 'iut')
+    data = get_sensor_data(sensor_id, location)
     if data is None:
         return {"error": "Sensor not found"}, 404
     return jsonify(data), 200
 
 @app.route("/api/room/<room>/sensors")
 def get_data_sensors_by_room(room):
-    sensors = get_sensors_list_for_room(room)
+    location = request.args.get('location', 'iut')
+    sensors = get_sensors_list_for_room(room, location)
     if sensors is None:
         return {"error": "Room not found"}, 404
     
@@ -135,7 +192,7 @@ def get_data_sensors_by_room(room):
     for sensor_id in sensors:
         sensor_type = sensor_id.split('_', 1)[1]
         if any(sensor_type == t for t, _ in TYPESENSOR):
-            data = get_sensor_data(sensor_id)
+            data = get_sensor_data(sensor_id, location)
             if data:
                 result[sensor_type] = data
 
@@ -143,36 +200,36 @@ def get_data_sensors_by_room(room):
 
 @app.route("/api/room/<room>/occupancy")
 def get_room_occuped(room):
-    current_time = int(time.time())
-    three_minutes = 3 * 60
-    is_occupied = (current_time // three_minutes) % 2 == 0
-    
-    return jsonify({"isOccuped": is_occupied})
-
-THRESHOLDS = {
-    "co2_level": 1000,
-    "temperature": (20, 26),
-    "humidity": (30, 60),
-    "loudness": 50,
-    "smoke_density": 0
-}
+    location = request.args.get('location', 'iut')
+    is_occupied = False
+    if location == IUT_LOCATION:
+        if room not in IUT_ROOMS:
+            return {"error": "Room not in list of room"}, 404
+        if IUT_ROOMS.index(room) % 2 == 0:
+            is_occupied = True
+    else: 
+        if room not in TETRAS_ROOMS:
+            return {"error": "Room not in list of room"}, 404
+        if TETRAS_ROOMS.index(room) % 2 == 1:
+            is_occupied = True
+    return jsonify(is_occupied), 200
 
 def detect_discomfort(name, value):
     discomfort = {"status": False, "causes": None}
 
     if 'co2_level' in name:
-        if value > THRESHOLDS["co2_level"]:
+        if random.randint(0,1)==1:
             discomfort["status"] = True
             discomfort["causes"] = "CO2 élevé"
-    elif 'temperature' in name:
+    elif random.randint(0,1)==1:
         if value < THRESHOLDS["temperature"][0] or value > THRESHOLDS["temperature"][1]:
             discomfort["status"] = True
             discomfort["causes"] = "Température inconfortable"
-    elif 'humidity' in name:
+    elif random.randint(0,1)==1:
         if value < THRESHOLDS["humidity"][0] or value > THRESHOLDS["humidity"][1]:
             discomfort["status"] = True
             discomfort["causes"] = "Humidité inconfortable"
-    elif 'loudness' in name:
+    elif random.randint(0,1)==1:
         if value > THRESHOLDS["loudness"]:
             discomfort["status"] = True
             discomfort["causes"] = "Niveau de bruit élevé"
